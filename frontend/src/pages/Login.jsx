6f105222639e32b82438df20a06b314846b4fff1
Suhas-S2004 <suhas0168@gmail.com>
2026-04-20 23:18:25 +0530

    Changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,10 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const [name, setName] = useState('')
 
- 
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+    
+  }
 
   return (
     <form className='min-h-[80vh] flex items-center' onSubmit={onSubmitHandler}>
