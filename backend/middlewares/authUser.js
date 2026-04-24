2b38b0de881828c8e74af7546450396ca927134a
Suhas-S2004 <suhas0168@gmail.com>
2026-04-24 20:34:13 +0530

    changes

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -11,7 +11,8 @@ const authUser = async (req, res, next) => {
 
     const token_decode = jwt.verify(token, process.env.JWT_SECRET)
 
-    req.body.userId =token_decode.id
+    req.body = req.body || {}           // ← ensure body exists
+    req.body.userId = token_decode.id
 
     
     next()
