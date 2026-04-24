c02afbff96f3c105e64642b0c507e9f63c7601f4
Suhas-S2004 <suhas0168@gmail.com>
2026-04-24 19:31:32 +0530

    changes

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -10,11 +10,9 @@ const AppContextProvider = (props) => {
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL
   const [doctors,setDoctors] = useState([])
+  const [token,setToken] =useState(localStorage.getItem('token')?localStorage.getItem('token'):false)
 
-  const value = {
-    doctors,
-    currencySymbol
-  }
+  
 
 const getDoctorsData = async () => {
 
@@ -34,6 +32,13 @@ const getDoctorsData = async () => {
     }
 
 }
+
+const value = {
+    doctors,
+    currencySymbol,
+    token,setToken,
+    backendUrl
+  }
 
 useEffect(()=>{
   getDoctorsData()
