3906e3f30b00852164f923f2dd4eef2ee4311930
Suhas-S2004 <suhas0168@gmail.com>
2026-04-26 15:29:27 +0530

    Changes

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -9,6 +9,7 @@ const DoctorContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [dToken,setDToken] = useState(localStorage.getItem('dToken')? localStorage.getItem('dToken') : '')
     const [appointments,setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
 
     const getAppointments = async () =>{
         try {
@@ -69,6 +70,22 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+            const {data} =await axios.get(backendUrl +'/api/doctor/dashboard', {headers:{dToken}})
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData)
+            } else{
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+             console.log(error);
+            toast.error(error.message)
+        }
+    }
+
 
     const value = {
         dToken,setDToken,
@@ -76,7 +93,8 @@ const DoctorContextProvider = (props) => {
         appointments,setAppointments,
         getAppointments,
         completeAppointment,
-        cancelAppointment
+        cancelAppointment,
+        dashData,setDashData,getDashData
 
     }
 
